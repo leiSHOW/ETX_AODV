50568d0de484e5f1f2ff66e711bba2c607294052
leiSHOW <95155014+leiSHOW@users.noreply.github.com>
2025-01-10 15:42:35 +0800

    Update aodv-neighbor-etx.cc

--- aodv/model/aodv-neighbor-etx.cc
+++ aodv/model/aodv-neighbor-etx.cc
@@ -12,6 +12,7 @@ NS_LOG_COMPONENT_DEFINE ("AodvNeighborEtx");
 namespace aodv
 {
 
+//将LPP时间戳初始化为0，时间戳是0-11循环的
 NeighborEtx::NeighborEtx () : m_lppTimeStamp (0) {}
 
 uint8_t
@@ -25,32 +26,32 @@ NeighborEtx::CalculateNextLppTimeStamp (uint8_t currTimeStamp)
   return nextTimeStamp;
 }
 
-void
-NeighborEtx::GotoNextLppTimeStamp ()
+
+//根据当前的时间戳 currTimeStamp 计算下一个时间戳
+void NeighborEtx::GotoNextLppTimeStamp ()
 {
   m_lppTimeStamp = CalculateNextLppTimeStamp (m_lppTimeStamp);
 }
 
-uint8_t 
-NeighborEtx::Lpp10bMapToCnt (uint16_t lpp10bMap)
+//将LPP包头中的10位映射（lpp10bMap）转换为计数值【通过检查每个位，计算在当前时间戳和下一个时间戳之外的有效LPP计数】
+uint8_t NeighborEtx::Lpp10bMapToCnt (uint16_t lpp10bMap)
 {
   uint8_t lpp = 0;
   for (int j=0; j<12; ++j)
     {
       if ((j!=m_lppTimeStamp) && (j!=(m_lppTimeStamp+1)))
         {
-          lpp += (lpp10bMap & ((uint16_t)0x0001 << j)) ? 1 : 0;
+          lpp += (lpp10bMap & ( (uint16_t)0x0001 << j) ) ? 1 : 0;
         }
     }
   return lpp;
 }
 
-void
-NeighborEtx::GotoNextTimeStampAndClearOldest ()
+//更新到下一个时间戳，并清除最旧的时间戳的LPP计数数据
+//CalculateNextLppTimeStamp获取下一个LPP时间戳
+void NeighborEtx::GotoNextTimeStampAndClearOldest ()
 {
   GotoNextLppTimeStamp (); // go to  next timestamp which becomes current timestamp
-  // Clear oldest timestamp lpp count values, this is next time stamp compared to current
-  //NS_LOG_UNCOND ("ETX: " << Simulator::Now ().GetSeconds () << ", " << (uint16_t)m_lppTimeStamp << ", " << (uint16_t)(~((uint16_t)0x0001 << CalculateNextLppTimeStamp (m_lppTimeStamp))));
   for (std::map<Ipv4Address, Etx>::iterator i = m_neighborEtx.begin (); i != m_neighborEtx.end (); ++i)
     {
       Etx etx = i->second;
@@ -63,22 +64,21 @@ NeighborEtx::GotoNextTimeStampAndClearOldest ()
     }  
 }
 
-void
-NeighborEtx::FillLppCntData (LppHeader &lppHeader)
+//将邻居的LPP计数数据填充到LPP包头中
+//对于每个邻居，计算其有效的LPP计数，并将其添加到LPP包头中的邻居列表
+void NeighborEtx::FillLppCntData (LppHeader &lppHeader)
 {
   for (std::map<Ipv4Address, Etx>::iterator i = m_neighborEtx.begin (); i != m_neighborEtx.end (); ++i)
         {
           uint8_t lpp = Lpp10bMapToCnt (i->second.m_lppMyCnt10bMap);
-          if (lpp > 0)
-            {
-              lppHeader.AddToNeighborsList (i->first, lpp);
-              //NS_LOG_UNCOND ("           IP=" << i->first << ", lpp=" << (uint16_t)lpp << ", rev=" << (uint16_t)(i->second.m_lppReverse) << ", ETX-bin-shift=" << CalculateBinaryShiftedEtx (i->second));
-            }
+          if (lpp > 0) lppHeader.AddToNeighborsList (i->first, lpp);
         }
 }
 
-bool 
-NeighborEtx::UpdateNeighborEtx (Ipv4Address addr, uint8_t lppTimeStamp, uint8_t lppReverse)
+
+//更新邻居的ETX数据
+//如果该邻居不存在，则插入新数据。如果已存在，则更新其LPP计数和反向计数
+bool NeighborEtx::UpdateNeighborEtx (Ipv4Address addr, uint8_t lppTimeStamp, uint8_t lppReverse)
 {
   std::map<Ipv4Address, Etx>::iterator i = m_neighborEtx.find (addr);
   if (i == m_neighborEtx.end ())
@@ -99,8 +99,9 @@ NeighborEtx::UpdateNeighborEtx (Ipv4Address addr, uint8_t lppTimeStamp, uint8_t 
     }
 }
 
-uint32_t 
-NeighborEtx::CalculateBinaryShiftedEtx (Etx etxStruct)
+//计算ETX值，基于链路的LPP计数和反向计数
+//ETX的计算使用LPP计数和反向计数的乘积来评估链路的质量：链路成功传输的成功率越高，ETX值越低
+uint32_t NeighborEtx::CalculateBinaryShiftedEtx (Etx etxStruct)
 {
   uint32_t etx = UINT32_MAX;
   if ((Lpp10bMapToCnt (etxStruct.m_lppMyCnt10bMap)!=0) && (etxStruct.m_lppReverse!=0))
@@ -111,8 +112,10 @@ NeighborEtx::CalculateBinaryShiftedEtx (Etx etxStruct)
   return etx;
 }
 
-uint32_t 
-NeighborEtx::GetEtxForNeighbor (Ipv4Address addr)
+
+//获取指定邻居的ETX值
+//如果该邻居的ETX数据已存储在邻居表中，则返回对应的ETX值；如果该邻居不存在，则返回最大值UINT32_MAX，表示链路质量极差或不可用
+uint32_t NeighborEtx::GetEtxForNeighbor (Ipv4Address addr)
 {
   uint32_t etx;
   std::map<Ipv4Address, Etx>::iterator i = m_neighborEtx.find (addr);
