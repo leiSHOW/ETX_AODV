41d4fe7a4eaa6efb750f23b9916fec8f4c7a9ca9
leiSHOW <95155014+leiSHOW@users.noreply.github.com>
2025-01-10 14:36:12 +0800

    Update aodv-neighbor-etx.h

--- aodv/model/aodv-neighbor-etx.h
+++ aodv/model/aodv-neighbor-etx.h
@@ -1,31 +1,3 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-/*
- * Copyright (c) 2009 IITP RAS
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Based on 
- *      NS-2 AODV model developed by the CMU/MONARCH group and optimized and
- *      tuned by Samir Das and Mahesh Marina, University of Cincinnati;
- * 
- *      AODV-UU implementation by Erik Nordström of Uppsala University
- *      http://core.it.uu.se/core/index.php/AODV-UU
- *
- * Authors: Nenad Jevtic <n.jevtic@sf.bg.ac.rs>, <nen.jevtc@gmail.com>
- *          Marija Malnar <m.malnar@sf.bg.ac.rs>
- */
- 
 #ifndef AODVNEIGHBORETX_H
 #define AODVNEIGHBORETX_H
 
@@ -49,7 +21,7 @@ public:
     Etx () : m_lppMyCnt10bMap (0), m_lppReverse (0) {}
   };
   
-  // Returns current time stamp (it is needed for sending LPP packet, used as LPP ID)
+  //Returns current time stamp (it is needed for sending LPP packet, used as LPP ID)
   uint8_t GetLppTimeStamp () {return m_lppTimeStamp; }
   // This function is used to prepare for new cicle of sending LPP packets.
   // It clears oldest LPP count data and moves to the new time stamp.
